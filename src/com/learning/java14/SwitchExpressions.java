bc37a100355419ca93cc0d32445cd9d3582a7722
grijeshsaini <grijesh.rajoria@gmail.com>
2021-10-25 00:13:34 +0100

    Reformatting.

--- src/com/learning/java14/SwitchExpressions.java
+++ src/com/learning/java14/SwitchExpressions.java
@@ -2,10 +2,6 @@ package com.learning.java14;
 
 public class SwitchExpressions {
 
-    enum Month {
-        JANUARY, FEBRUARY, MARCH, APRIL, MAY, JUNE, JULY, AUGUST, SEPTEMBER, OCTOBER, NOVEMBER, DECEMBER
-    }
-
     public static void main(String[] args) {
         var month = Month.OCTOBER;
         System.out.println(getSeason(month));
@@ -15,26 +11,30 @@ public class SwitchExpressions {
     private static String getSeason(Month month) {
         //No break keyword, cases don’t fall-through
         return switch (month) {
-            case  DECEMBER, JANUARY, FEBRUARY -> "Winter";
-            case  MARCH, APRIL, MAY -> "Spring";
-            case  JUNE, JULY, AUGUST -> "Summer";
-            case  SEPTEMBER, OCTOBER, NOVEMBER -> "Autumn";
+            case DECEMBER, JANUARY, FEBRUARY -> "Winter";
+            case MARCH, APRIL, MAY -> "Spring";
+            case JUNE, JULY, AUGUST -> "Summer";
+            case SEPTEMBER, OCTOBER, NOVEMBER -> "Autumn";
         };
     }
 
     private static int getDiscountForMonth(Month month) {
         return switch (month) {
-            case  DECEMBER, JANUARY, FEBRUARY -> {
+            case DECEMBER, JANUARY, FEBRUARY -> {
                 int monthLength = month.toString().length();
                 yield monthLength * 4;
             }
-            case  MARCH, APRIL, MAY -> {
+            case MARCH, APRIL, MAY -> {
                 int monthLength = month.toString().length();
                 yield monthLength * 2;
             }
             // If all the cases are not covered then default is mandatory.
             default -> 10;
         };
+    }
+
+    enum Month {
+        JANUARY, FEBRUARY, MARCH, APRIL, MAY, JUNE, JULY, AUGUST, SEPTEMBER, OCTOBER, NOVEMBER, DECEMBER
     }
 
 }
